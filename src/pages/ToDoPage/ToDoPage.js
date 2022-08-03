18fae0c3110bb579ddc2789c8bfd6587f668b23c
Alena.Kygai <alena.kygai@gmail.com>
2022-08-03 20:56:49 +0300

    import hook useToDo & passing data

--- src/pages/ToDoPage/ToDoPage.js
+++ src/pages/ToDoPage/ToDoPage.js
@@ -2,13 +2,28 @@ import React from 'react';
 import Header from 'components/Header';
 import styles from './ToDoPage.module.scss';
 import ToDoForm from 'components/ToDo/ToDoForm';
+import ToDoList from 'components/ToDo/ToDoList';
+import useTodo from '../../hooks/useToDo';
 
 const ToDoPage = () => {
+    const {
+    tasks,
+    addTask,
+    removeTask,
+    changeStatus,
+    onFilter,
+  } = useTodo();
+  
   return(
     <>
     <Header />
     <h2 className={styles.toDoPageTitle}>Unusual Todo List</h2>
-    <ToDoForm />
+    <ToDoForm addTask={addTask} onFilter={onFilter}/>
+    <ToDoList 
+      tasks={tasks} 
+      removeTask={removeTask} 
+      changeStatus={changeStatus}
+    />
     </>
   )
 }
