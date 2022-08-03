0c8e1cc15786b09e27ab1653070d2e2ddcb5cc8d
Alena.Kygai <alena.kygai@gmail.com>
2022-08-03 18:47:35 +0300

    Refactor validation schema for bodyTask

--- src/utils/schemaValidations.js
+++ src/utils/schemaValidations.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
 
 export const TASK_SCHEMS = Yup.object({
- bodyTask: Yup.string().matches(/[A-Z][a-z]{10,100}$/,'Must be latin letter').required(),
+ bodyTask: Yup.string().matches(/[A-Z,a-z]{5,100}$/,'Must be latin letter').required('Please enter the required field'),
 })
